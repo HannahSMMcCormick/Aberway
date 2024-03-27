23293b2f8ac6f0050a6e50773848b0ecbd2d8066
HannahSMMcCormick <114184694+HannahSMMcCormick@users.noreply.github.com>
2024-03-27 18:11:45 +0000

    part a part something

--- HannahMcCormick__AberWay.py
+++ HannahMcCormick__AberWay.py
@@ -2,6 +2,8 @@
 
 from aberway_background_code import create, update, main_loop
 import time
+import math
+import pygame 
 
 ColourFlip = False
 
@@ -17,10 +19,52 @@ length = 676.75
 error = 0.06
 
 def path_update():
-    ListOfNodeId = [] #set the value of this to the nodes that your path takes
+    ListOfNodeId = [0,2,10,13,11,14,15] #set the value of this to the nodes that your path takes
     start = time.time_ns() # for timing your algorithm
     # ---------- ---------- YOUR CODE GOES HERE ---------- ----------
 
+    Journey = []
+
+    currentNode = startPos
+
+    endNode = listOfNodesToPass[-1]
+
+    distance = math.dist(nodeList[startPos][0],nodeList[endNode][0])
+
+    #print(distance)#
+
+    shortNode = None
+
+    testDistance = distance
+
+
+    while currentNode != endNode:
+
+
+        for i in range(len(nodeList[currentNode][3])):
+
+            testNode = nodeList[currentNode][3][i]
+
+            tempDistance = math.dist(nodeList[testNode][0], nodeList[endNode][0])
+
+            if tempDistance < testDistance:
+
+                testDistance = tempDistance
+
+                shortNode = testNode
+            
+
+        if shortNode is not None:
+
+            currentNode = shortNode
+
+            Journey.append(currentNode)
+
+        else:
+
+            print("ERROR")
+
+    print(Journey)
 
 
 
